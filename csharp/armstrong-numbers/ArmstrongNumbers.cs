831ba28ad3d276165f414069c4e606f9849a18a2
Daniel H da Silva <danielhensilva@gmail.com>
2017-12-28 23:08:46 +0000

    update

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        var decompose = number;
+        var length = 0;
+
+        while (decompose > 0)
+        {
+            decompose = decompose / 10;
+            length++;
+        }
+
+        var sum = 0d;
+        decompose = number;
+
+        while (decompose > 0)
+        {
+            var digit = decompose % 10;
+            sum += Math.Pow(digit, length);
+            decompose = decompose / 10;
+        }
+
+        return number == sum;
+    }
+}
