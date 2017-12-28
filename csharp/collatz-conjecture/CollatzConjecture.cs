9e3a361a169c72850c62b67bc0b1df1021d5905f
Daniel H da Silva <danielhensilva@gmail.com>
2017-12-28 18:56:15 +0000

    update

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number <= 0)
+            throw new ArgumentException();
+
+        if (number == 1)
+            return 0;
+
+        if (number % 2 == 0)
+            return 1 + CollatzConjecture.Steps(number / 2);
+
+        else
+            return 1 + CollatzConjecture.Steps(3 * number + 1); 
+    }
+}
