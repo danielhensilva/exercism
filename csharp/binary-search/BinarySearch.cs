2d6d8f7f95ed6a2838bde14d0a5774d84adecc59
Daniel H da Silva <danielhensilva@gmail.com>
2017-12-29 13:03:51 +0000

    update

---
+++ csharp/binary-search/BinarySearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class BinarySearch
+{
+    private int[] input;
+
+    public BinarySearch(int[] input)
+    {
+        this.input = input;
+    }
+
+    public int Find(int value)
+    {
+        var lower = 0;        
+        var upper = this.input.Length - 1;
+
+        while (lower <= upper) 
+        {
+            var i = (upper + lower) / 2;
+
+            if (value == this.input[i])
+                return i;
+
+            if (value < this.input[i])
+                upper = i - 1;
+            else
+                lower = i + 1;
+        }
+
+        return -1;
+    }
+}
