076cd070801201100f455a33310be5c4de675a95
Daniel H da Silva <danielhensilva@gmail.com>
2018-10-29 14:34:40 +0000

    update

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -3,8 +3,11 @@ from collections import Counter
 
 def detect_anagrams(word, candidates):
     anagrams = []
-    word_counter = Counter(word.lower())
+    word = word.lower()
+    word_counter = Counter(word)
     for candidate in candidates:
+        if (candidate.lower() == word):
+            continue
         candidate_counter = Counter(candidate.lower())
         if word_counter == candidate_counter:
             anagrams.append(candidate)
