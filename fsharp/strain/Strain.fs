2cf35b47b13e9533f2aea5f65df4a53d28073521
Daniel H da Silva <danielhensilva@gmail.com>
2019-07-06 07:28:33 +0100

    Update

---
+++ fsharp/strain/Strain.fs
@@ -0,0 +1,17 @@
+﻿module Seq
+
+let rec keep pred xs =
+    let mutable x = []
+    for i in xs do
+        match pred(i) with
+        | true -> x <- x @ [i]
+        | _ -> ignore i
+    x
+
+let rec discard pred xs =
+    let mutable x = []
+    for i in xs do
+        match pred(i) with
+        | false -> x <- x @ [i]
+        | _ -> ignore i
+    x
