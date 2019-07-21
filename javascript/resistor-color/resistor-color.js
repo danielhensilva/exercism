e9e2024b76161be4e32bee44c9cde1417fb93dd7
Daniel H da Silva <danielhensilva@gmail.com>
2019-07-21 07:08:25 +0100

    Update

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,16 @@
+export const colorCode = (target) => {
+  return COLORS.indexOf(target);
+};
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"
+];
