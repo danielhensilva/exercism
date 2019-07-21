2fcb36871ea7bb634e5a989e6aef7ea206f02b9d
Daniel H da Silva <danielhensilva@gmail.com>
2019-07-21 07:30:47 +0100

    Update

---
+++ javascript/raindrops/raindrops.js
@@ -0,0 +1,16 @@
+export const convert = (number) => {
+  let result = '';
+  if (number % 3 === 0) {
+    result += 'Pling';
+  }
+  if (number % 5 === 0) {
+    result += 'Plang';
+  }
+  if (number % 7 === 0) {
+    result += 'Plong';
+  }
+  if (result.length === 0) {
+    result += number;
+  }
+  return result;
+};
