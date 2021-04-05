3f5f9da97fc9e2c8e2886133ca9368cf55967a13
Daniel H da Silva <danielhensilva@gmail.com>
2021-04-05 16:38:12 +0100

    Update

---
+++ fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -0,0 +1,21 @@
+﻿module CollatzConjecture
+
+
+let addOne (number: int option) : int option =
+    match number with
+    | Some x -> Some(x + 1)
+    | None -> None
+
+
+let nextNumber (number: int) : int =
+    if (number % 2 = 0) then
+        (number / 2)
+    else
+        (number * 3 + 1)
+
+
+let rec steps (number: int) : int option =
+    match number with
+    | n when n <= 0 -> None
+    | n when n = 1 -> Some 0
+    | _ -> number |> nextNumber |> steps |> addOne
